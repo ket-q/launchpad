dffc98ce46c9babe35aacae2042ed16cc8e663f6
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-02-09 22:34:34 +0900

    added

---
+++ dev_tools.ps1
@@ -0,0 +1,17 @@
+Write-Host "Installer for development tools... :)"
+
+Disable-UAC
+
+Write-Host "Installing VSCode"
+choco install -y vscode.install --params "/NoDesktopIcon"
+
+Write-Host "Installing VSCode Python extension"
+choco install -y vscode-python
+
+Write-Host "Installing VSCode Jupyter extension"
+choco install -y vscode-jupyter
+
+Write-Host "Installing Microsoft Windows Terminal"
+choco install -y microsoft-windows-terminal
+
+Enable-UAC
