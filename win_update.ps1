d9af47814eb19b8c4b83c5f284d9e567bb7d871d
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-02-09 20:56:51 +0900

    ...

--- win_update.ps1
+++ win_update.ps1
@@ -5,7 +5,7 @@ Disable-UAC
 # Install all available win updates
 Write-Host "Install available Windows updates..."
 Enable-MicrosoftUpdate
-Install-WindowsUpdate -acceptEula -GetUpdatesFromMS
+Install-WindowsUpdate -acceptEula
 
 # Re-enable UAC (security)
 Enable-UAC
