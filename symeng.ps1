3fb422082e70113de14c8dab45e5502f675ead74
Bernd Burgstaller <9303437+berndburgstaller@users.noreply.github.com>
2025-02-21 23:43:27 +0900

    .virtualenvs

--- symeng.ps1
+++ symeng.ps1
@@ -1,3 +1,5 @@
-python -m venv myvenv
-.\myvenv\Scripts\Activate.ps1
+$DOT_VENVS_DIR = Join-Path ${env:USERPROFILE} -ChildPath '.virtualenvs'
+$VENV_DIR = Join-Path $DOT_VENVS_DIR -ChildPath 'symeng_venv'
+python -m venv ${VENV_DIR}
+${VENV_DIR}\Scripts\Activate.ps1
 pip install symengine==0.13.0
